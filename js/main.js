245c61dc637983549ac5379c843873e94249ec09
SirBob <kleonardo52@gmail.com>
2017-10-20 17:05:10 +0800

    added modal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+var menu_trigger = document.getElementById('modal-icon')
+var menu_popup = document.getElementById('main-menu')
+var menu_close = document.getElementById("main-menu-close")
 
+alert(menu_trigger)
+menu_trigger.onclick = function () {
+	menu_popup.style.display = 'block'
+}
+
+menu_close.onclick = function () {
+	menu_popup.style.display = 'none'
+}
